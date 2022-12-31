3bd02e73c2e1154acb999ef5e10b9e6d5722777a
dmora219 <dmora14633@gmail.com>
2022-12-31 13:09:55 -0600

    changed docter strange and added cover to the background images

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let thor = document.getElementById('thor');
+/*let thor = document.getElementById('thor');
 let spiderman = document.getElementById('spiderman');
 let hulk = document.getElementById('hulk');
 let ironman = document.getElementById('ironman');
@@ -30,5 +30,23 @@ strange.addEventListener('click', function () {
    document.querySelector('body').style.backgroundImage = 'url(strange.jpg)'
    document.querySelector('body').style.backgroundSize = '600px';
 })
+*/
 
 
+const elements = [
+  {id: 'thor', url: './thor.png'},
+  {id: 'spiderman', url: './spiderman.jpg'},
+  {id: 'hulk', url: './hulk.jpg'},
+  {id: 'ironman', url: './ironman.jpg'},
+  {id: 'panther', url: './panther.jpg'},
+  {id: 'strange', url: './strange2.jpg'}
+];
+
+for (const element of elements) {
+   const elem = document.getElementById(element.id);
+   elem.addEventListener('click', function() {
+     document.querySelector('body').style.backgroundImage = `url(${element.url})`;
+     document.querySelector('body').style.backgroundSize = 'cover';
+     
+   });
+ }
